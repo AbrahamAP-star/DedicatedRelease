b3a3a3fd913d1b8478e75c9a848466f576bdea50
Abraham Code <fuentesabraham075@gmail.com>
2026-02-15 16:32:13 +0100

    py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DedicatedRelease",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Abraham Code",                         # Tu nombre
+    author_email="fuentessabraham@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/AbrahamAP-star/DedicatedRelease",     # URL del proyecto
+)
